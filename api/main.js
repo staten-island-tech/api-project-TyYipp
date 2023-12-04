ced667e84fce5697098dd9540d5dc52e8f967ebb
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-12-04 10:11:29 -0500

    12/4
    getting array to print in console but can't print individual information

--- api/main.js
+++ api/main.js
@@ -1,20 +1,22 @@
 import './style.css'
+//https://botw-compendium.herokuapp.com/api/v3/compendium/all
+//https://www.amiiboapi.com/api/
+const URL = 'https://www.amiiboapi.com/api/amiibo/?name=mario';
+// API (Application Programming Interface) - if you want to get data
+// from the internet programmatically, it's likely that you will have
+// to deal with an API.
 
+// Below is an entry point to an api that generates random quotes.
+// APIs are usually accessible with a HTML/website link.
+// (If you open this in a browser, you will get raw object data.)
+const apiEntry = "https://www.amiiboapi.com/api/amiibo/?name=mario";
 
-const URL = 'https://botw-compendium.herokuapp.com/api/v3/compendium/all';
-async function getData(URL) {
-  try {
-//requesitong a response REST API's
-      const response = await fetch(URL)
-      if (response.status != 200) {
-      throw new Error(response.statusText);
-      }
-      //convert response to json
-      const data = await response.json();
-      document.querySelector("h1").textContent = data.content;
-  } catch (error) {
-      console.log(error, "Uh Oh spagettios")
-      document.querySelector("h1").textContent = '💀Error 404🤖';
-  }
-}
-getData(URL);
+// fetch is a function (that you've seen previously) that can retrieve
+// data from an api entry point.
+console.log(fetch(apiEntry));
+
+// fetch() returns a "response", which we must convert into a object json format
+fetch(apiEntry)
+    .then((response) => response.json()) // use the `.json()` method
+  // `.json()` is also async, chain another `.then()` to log the object
+.then(data.forEach((dat) => console.log(dat.character)))
