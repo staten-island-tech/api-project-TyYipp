d0e445e8d60cac4410e50f5a81ef87aa12824b79
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-12-05 10:13:51 -0500

    12/5
    trying to for each api

--- api/main.js
+++ api/main.js
@@ -1,22 +1,13 @@
 import './style.css'
 //https://botw-compendium.herokuapp.com/api/v3/compendium/all
 //https://www.amiiboapi.com/api/
-const URL = 'https://www.amiiboapi.com/api/amiibo/?name=mario';
-// API (Application Programming Interface) - if you want to get data
-// from the internet programmatically, it's likely that you will have
-// to deal with an API.
 
-// Below is an entry point to an api that generates random quotes.
-// APIs are usually accessible with a HTML/website link.
-// (If you open this in a browser, you will get raw object data.)
-const apiEntry = "https://www.amiiboapi.com/api/amiibo/?name=mario";
 
-// fetch is a function (that you've seen previously) that can retrieve
-// data from an api entry point.
-console.log(fetch(apiEntry));
+const api = "https://botw-compendium.herokuapp.com/api/v3/compendium/all";
 
-// fetch() returns a "response", which we must convert into a object json format
-fetch(apiEntry)
-    .then((response) => response.json()) // use the `.json()` method
-  // `.json()` is also async, chain another `.then()` to log the object
-.then(data.forEach((dat) => console.log(dat.character)))
+console.log(fetch(api));
+
+
+fetch(api)
+    .then((response) => response.json()) 
+    .then((data) => console.log(data.data)); 
