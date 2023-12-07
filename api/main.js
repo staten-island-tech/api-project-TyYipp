45879f7d287ebadae25232be59d184233becaba0
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-12-07 10:12:07 -0500

    12/7
    deleted counter.js cause yes
    found out how to for each api array
    data.data bc data is the name of the array
    thx ricky!

--- api/main.js
+++ api/main.js
@@ -1,20 +1,31 @@
 import './style.css'
 //https://botw-compendium.herokuapp.com/api/v3/compendium/all
 //https://www.amiiboapi.com/api/
-
-
+const DOMSelectors = {
+    parent: document.querySelector(".parent")
+}
 const api = "https://botw-compendium.herokuapp.com/api/v3/compendium/all";
 async function getData() {
 try {
             const response = await fetch(api)
             const data = await response.json()
-            document.querySelector("h1").textContent = data.content;
+            data.data.forEach((data) => {
+                let name = data.name;
+            
+            const card = 
+            `<div id="child>
+            <h1>${name}</h1>
+            </div>`
+            DOMSelectors.parent.insertAdjacentHTML('beforeend', card);
+            });
     } catch (error) {
         console.log(error, "Uh Oh spagettios")
-        document.querySelector("h1").textContent = '💀Error 404🤖';
+        document.querySelector(".stuff").textContent = '💀Error 404🤖';
+
     }
 }
-getData();
+getData(api);
+
 /* fetch(api)
     .then((response) => response.json()) 
     .then(data => {
