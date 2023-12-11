407e54183f26c16f64a1fda0e3547830821b177f
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-12-11 10:15:01 -0500

    12/11
    designing css and html

--- api/main.js
+++ api/main.js
@@ -7,17 +7,26 @@ const DOMSelectors = {
 const api = "https://botw-compendium.herokuapp.com/api/v3/compendium/all";
 async function getData() {
 try {
-            const response = await fetch(api)
-            const data = await response.json()
-            data.data.forEach((data) => {
-                let name = data.name;
-            
-            const card = 
-            `<div id="child>
-            <h1>${name}</h1>
-            </div>`
-            DOMSelectors.parent.insertAdjacentHTML('beforeend', card);
-            });
+    const response = await fetch(api)
+    const data = await response.json()
+    console.log(data)
+                function insertCards(data) {
+                    DOMSelectors.parent.innerHTML = '';
+                    data.data.forEach((data) => {
+                      let name = data.name;
+                      let img = data.image;
+                      let id = data.id;
+                      const card = `
+                        <div id="child">
+                          <h2 id="text">Name: ${name}</h2>
+                          <img src="${img}" alt="" class="img">
+                          <h3 id="text">Type: ${id}</h3>
+                        </div>
+                      `;
+                      DOMSelectors.parent.insertAdjacentHTML('beforeend', card);
+                    });
+                    };
+                    insertCards()
     } catch (error) {
         console.log(error, "Uh Oh spagettios")
         document.querySelector(".stuff").textContent = '💀Error 404🤖';
