db6633271a0fe72b155860e03a09032d4f58def8
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-12-21 09:45:21 -0500

    12/21
    filter works yay! thx ricky li

--- api/main.js
+++ api/main.js
@@ -1,48 +1,47 @@
 import './style.css'
 
 const api = "https://botw-compendium.herokuapp.com/api/v3/compendium/all";
+
+const DOMSelectors = {
+  parent: document.querySelector(".parent")
+}
+function clearfields() {
+  DOMSelectors.parent.innerHTML = '';
+}
+
 async function getData() {
 try {
-  const DOMSelectors = {
-    parent: document.querySelector(".parent")
-}
     const response = await fetch(api)
     const data = await response.json()
-    const hi = data.data.forEach((data) => data.category);
-    console.log(hi)
-              function insertCards(data) {
-                  DOMSelectors.parent.innerHTML = '';
-                  const sortid = data.data.sort((a,b) => a.id - b.id);
-                  sortid.forEach((data) => {
-                        let name = data.name;
-                        let img = data.image;
-                        let id = data.id;
-                        const card = `
-                          <div class="child">
-                            <h2 class="text">Name: ${name}</h2>
-                            <img src="${img}" alt="" class="img">
-                            <h3 class="text">Type: ${id}</h3>
-                          </div>
-                        `;
-                        DOMSelectors.parent.insertAdjacentHTML('beforeend', card);
-                      });
-                      };
-                      insertCards(data)
+    const sortid = data.data.sort((a,b) => a.id - b.id);
+    function insertCards(data){
+      data.forEach((card) => {
+              const all = 
+              `<div class="card">
+                  <h1 class="text">${card.name}</h1>
+                  <img src="${card.image}" class="img">
+                  <h4 class="text"> ${card.id}</h4>
+              </div>`
+              DOMSelectors.parent.insertAdjacentHTML("beforeend", all)
+      });
+  }
+  insertCards(sortid)
 
-                      let buttons = document.querySelectorAll('.btn');
-                      buttons.forEach((btn) =>
-                        btn.addEventListener('click', function (event) {
-                          event.preventDefault();
-                          let type = btn.getAttribute('alt').toLowerCase();
-                          let newArr = data.data.filter((data) => data.category === type);
-                          insertCards(newArr);
-                          console.log(newArr)
-                        }));
-    } catch (error) {
-        console.log(error, "Uh Oh spagettios")
-        document.querySelector(".stuff").textContent = '💀Error 404🤖';
+  let buttons = document.querySelectorAll('.btn');
+buttons.forEach((btn) =>
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+  clearfields();
+  let type = btn.getAttribute('alt').toLowerCase();
+  let newArr = data.data.filter((data) => data.category.toLowerCase() === type);
+  insertCards(newArr);
+  console.log(newArr)
+}));
+} catch (error) {
+console.log(error, "Uh Oh spagettios")
+document.querySelector(".stuff").textContent = '💀Error 404🤖';
 
-    }
-}
+    }};
+
 getData(api);
 
