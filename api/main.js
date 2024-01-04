813f2eb3c701faad69d39aac65140eb4c2622134
TyYipp <144461219+TyYipp@users.noreply.github.com>
2024-01-04 10:15:38 -0500

    12/4
    final changes today

--- api/main.js
+++ api/main.js
@@ -6,7 +6,6 @@ import './style.css'
 const api = "https://botw-compendium.herokuapp.com/api/v3/compendium/all";
 
 
-const counter = [1]
 
 const DOMSelectors = {
   parent: document.querySelector(".parent"),
@@ -22,19 +21,27 @@ function insertCards(data){
   data.forEach((card) => {
           const all =
           `<div class="card">
-              <h1 class="text">${card.name}</h1>
-              <img src="${card.image}" class="img" alt="${card.image}">
-              <h4 class="textd"> ${card.id}</h4>
+              <h1 class="text" alt="name">${card.name}</h1>
+              <img src="${card.image}" class="img" alt="${card.name} picture">
+              <h4 class="textd" alt="id"> ${card.id}</h4>
               <h2 class="textz"><br>${card.description}</h2>
-              ${card.common_locations ? `<h3 class="texty"><br><br><br>Common Locations<br>${card.common_locations}</h3>` : ''}
-              ${card.drops ? `<h4 class="textx"><br>Drops<br>${card.drops}</h4>` : ''}
+              ${card.common_locations ? `<h3 class="texty" alt="common location"><br><br><br>Common Locations<br>${card.common_locations} </h3>` : ''}
+              ${card.drops ? `<h4 class="textx" alt="drops"><br>Drops<br>${card.drops}</h4>` : ''}
               </div>`;
          
           DOMSelectors.parent.insertAdjacentHTML("beforeend", all)
   });
 }
-
-
+/* 
+const sortalp = data.data.sort(function (a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}); */
 
 
 
@@ -48,15 +55,7 @@ async function getData() {
     const response = await fetch(api);
     const data = await response.json();
     const sortid = data.data.sort((a, b) => a.id - b.id);
-    const sortalp = data.data.sort(function (a, b) {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
-    });
+
 
 
 
@@ -76,7 +75,7 @@ async function getData() {
        
       });
     });
-    insertCards(sortalp);
+    insertCards(sortid);
     expand(api);
   } catch (error) {
     console.log(error, "Uh Oh spagettios");
@@ -149,13 +148,13 @@ function imgclick(clickcard) {
   imgstyle.style.display = 'block';
   imgstyle.style.margin = 'auto';
   const zstyle = clickcard.querySelector('.textz');
-  zstyle.style.fontSize= '25px';
+  zstyle.style.fontSize= '30px';
   const ystyle = clickcard.querySelector('.texty');
   ystyle.style.fontSize= '25px';
   const xstyle = clickcard.querySelector('.textx');
   xstyle.style.fontSize= '25px';
   const dstyle = clickcard.querySelector('.textd');
-  dstyle.style.fontSize= '0.1px';
+  dstyle.style.fontSize= '40px';
 }};
 
 
@@ -183,7 +182,7 @@ async function home(api) {
 home(api)
 
 
-
+/* 
 async function toggle(api) {
   try {
   const response = await fetch(api);
@@ -200,16 +199,16 @@ async function toggle(api) {
   });
   DOMSelectors.sort.addEventListener('click', function(event) {
     event.preventDefault();
-    clearfields
-  if (counter[0] = 1)
+    clearfields();
+  if (counter[0] === 1)
   insertCards(sortid)
   counter.splice(0, 1, 0)
-  if (counter[0] = 0)
+  if (counter[0] === 0)
   insertCards(sortalp)
   counter.splice(0, 1, 1)
   })
   } catch {
     document.querySelector("h1").textContent = "Error 🤓🤓🤓";
   }
-}
+} */
 
